afbf301fd98f2a898551ec523c76eb88d352f3a6
torrijld <torrijld@dukes.jmu.edu>
2024-03-28 15:22:28 -0400

    viewcard additions
    :100:

--- scripts/createcard.js
+++ scripts/createcard.js
@@ -1,3 +1,35 @@
-const preview = document.getElementById("preview")
-const save = document.getElementById("save")
+document.getElementById("preview").addEventListener("click", function(event) {
+    var toValue = document.getElementById("to").value;
+    var fromValue = document.getElementById("from").value;
+    var titleValue = document.getElementById("title").value;
+    var subtitleValue = document.getElementById("subtitle").value;
+    var messageValue = document.getElementById("message").value;
 
+    document.querySelector(".to-text").textContent = toValue;
+    document.querySelector(".from-text").textContent = fromValue;
+    document.querySelector(".title-text").textContent = titleValue;
+    document.querySelector(".subtitle-text").textContent = subtitleValue;
+    document.querySelector(".message-text").textContent = messageValue;
+});
+
+document.getElementById("save").addEventListener("click", function(event) {
+    // event.preventDefault();
+
+    var cards = localStorage.getItem("cards");
+    if (cards === null) {
+        cards = [];
+    } else {
+        cards = JSON.parse(cards);
+    }
+
+    var currentCard = {
+        to: document.getElementById("to").value,
+        from: document.getElementById("from").value,
+        title: document.getElementById("title").value,
+        subtitle: document.getElementById("subtitle").value,
+        message: document.getElementById("message").value
+    };
+
+    cards.push(currentCard);
+    localStorage.setItem("cards", JSON.stringify(cards));
+});
--- scripts/viewcards.js
+++ scripts/viewcards.js
@@ -6,18 +6,30 @@ for (let i = 0; i < cards.length; i++) {
     let card = cards[i];
     console.log(card);
 
-    let cardView = template.textContent.cloneNode();
-    let titleText = cardView.querySelector(".ti")
+    let cardView = template.content.cloneNode(true);
+    let titleText = cardView.querySelector(".title");
+    let subtitleText = cardView.querySelector(".subtitle");
+    let toText = cardView.querySelector(".to");
+    let messageText = cardView.querySelector(".message");
+    let fromText = cardView.querySelector(".from");
+
     titleText.textContent = card.title;
+    subtitleText.textContent = card.subtitle;
+    toText.textContent = card.to;
+    messageText.textContent = card.message;
+    fromText.textContent = card.from;
 
     // TODO rest of text spans
 
     // Function is declared inside for loop, so it can be reached
     // If the func was declared after, it would 
-    // be called when the for loop ended.
+    // be called when the for loop ends.
     let deleteBtn = cardView.querySelector(".delete-btn")
     deleteBtn.addEventListener("click", function() {
-        console.log(i)
+        console.log(i);
+        cards.splice(i);
+        localStorage.setItem("cards", JSON.stringify(cards));
+        location.reload();
     })
 
     cardList.appendChild(cardView);
